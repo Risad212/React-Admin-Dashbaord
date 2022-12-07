a619ed6f0e15059166daf04dbc29c061b93546f2
Risad212 <hafez.risad@gmail.com>
2022-12-07 10:01:24 +0600

    chart create done

---
+++ src/Data/dummyData.js
@@ -0,0 +1,50 @@
+export const UserData = [
+    {
+        name: 'Jan',
+        "Active User": 4000,
+    },
+    {
+        name: 'Feb',
+        "Active User": 7800,
+    },
+    {
+        name: 'Mar',
+        "Active User": 2000,
+    },
+    {
+        name: 'Apr',
+        "Active User": 2780,
+    },
+    {
+        name: 'May',
+        "Active User": 1890,
+    },
+    {
+        name: 'Jun',
+        "Active User": 2390,
+    },
+    {
+        name: 'Jul',
+        "Active User": 9590,
+    },
+    {
+        name: 'Agu',
+        "Active User": 9790,
+    },
+    {
+        name: 'Sep',
+        "Active User": 3490,
+    },
+    {
+        name: 'Oct',
+        "Active User": 3490,
+    },
+    {
+        name: 'Nov',
+        "Active User": 3590,
+    },
+    {
+        name: 'Dec',
+        "Active User": 3990,
+    },
+];
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Chart from '../Components/chart/Chart';
 import FeatureInfo from '../Components/featureinfo/FeatureInfo';
 import './home.css';
+import { UserData } from '../Data/dummyData';
 
 const Home = () => {
     return (
         <div className='home'>
            <FeatureInfo />
+           <Chart data={UserData} title="User Analytics" grid dataKey='Active User'/>
         </div>
     );
 };
